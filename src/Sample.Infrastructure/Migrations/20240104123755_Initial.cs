b6023d77d2fc46f715cc02a75b6ba62461da578c
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2024-01-04 14:12:31 +0100

    migration

---
+++ src/Sample.Infrastructure/Migrations/20240104123755_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sample.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    parent_category_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_category", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_category_category_parent_category_id",
+                        column: x => x.parent_category_id,
+                        principalTable: "category",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_category_parent_category_id",
+                table: "category",
+                column: "parent_category_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "category");
+        }
+    }
+}
